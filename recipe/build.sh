328435eefd8f9580988b6c91d314a3d4b0929442
memento <claude.rc@gmail.com>
2024-01-24 11:38:48 -0600

    FIX: Right, but forgot the include files

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,7 +22,10 @@ make check
 make install
 
 if [[ "$target_platform" == win* ]]; then
-  mkdir -p ${PREFIX}/Library/bin ${PREFIX}/Library/lib ${PREFIX}/Library/lib/pkgconfig
+  mkdir -p ${PREFIX}/Library/bin ${PREFIX}/Library/include ${PREFIX}/Library/lib ${PREFIX}/Library/lib/pkgconfig
+
+  /usr/bin/install -c -m 644 ${PREFIX}/include/secp256k1*.h ${PREFIX}/Library/include
+
   /usr/bin/install -c -m 644 ${PREFIX}/lib/secp256k1.dll.lib ${PREFIX}/Library/lib/libsecp256k1.dll.lib
   /usr/bin/install -c -m 644 ${PREFIX}/lib/secp256k1.lib ${PREFIX}/Library/lib/libsecp256k1.lib
   /usr/bin/install -c -m 644 ${PREFIX}/lib/pkgconfig/libsecp256k1.pc ${PREFIX}/Library/lib/pkgconfig/libsecp256k1.pc
