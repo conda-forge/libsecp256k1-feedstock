3ed374fb975b8228b7e906feec2758f187d9485d
memento <claude.rc@gmail.com>
2024-02-20 16:46:14 -0600

    .

--- recipe/verify_pkg_config.py
+++ recipe/verify_pkg_config.py
@@ -1,25 +1,20 @@
-import re
-
-
 def main(*args):
     txt = args[0]
     words = args[1:]
 
-    with open(txt) as f:
-        found = False
-        for line in f:
-            line = line.replace('\\', '\\\\')
-            for w in words:
-                w = w.replace('\\', '\\\\')
-                if re.search(w, line):
-                    print(line)
+    for w in words:
+        with open(txt) as f:
+            found = False
+            for line in f:
+                if w in line:
+                    print('PASS')
                     found = True
                     break
-        if not found:
-            exit(1)
+            if not found:
+                exit(1)
 
 
 if __name__ == "__main__":
     import sys
-    
+
     main(*sys.argv[1:])
