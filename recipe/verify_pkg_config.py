90dfae2038e7d2dfdd44586a19d0ae14965b92a9
memento <claude.rc@gmail.com>
2024-02-21 16:48:58 -0600

    (fix) verify libsecp256k1.pc prefix value

---
+++ recipe/verify_pkg_config.py
@@ -0,0 +1,27 @@
+def main(*args):
+    txt = args[0]
+    words = args[1:]
+
+    for w in words:
+        w = w.strip()
+        w = w.replace('/', '-')
+        w = w.replace('\\', '-')
+        with open(txt) as f:
+            found = False
+            for line in f:
+                # Could contain / or \ as path separators
+                line = line.rstrip()
+                line = line.replace('/', '-')
+                line = line.replace('\\', '-')
+                if w in line:
+                    print(f'   {w[10:]} Found in {txt}')
+                    found = True
+                    break
+            if not found:
+                exit(1)
+
+
+if __name__ == "__main__":
+    import sys
+
+    main(*sys.argv[1:])
